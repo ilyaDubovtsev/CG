8633a52777aa6f01587335754e1b204b51c14f5f
ilyaDubovtsev <dubovtsev98@gmail.com>
2018-12-25 20:52:11 +0400

    Revert "hexagon fix"
    This reverts commit 269f7d6704776f3bb29192a0e4ab058e6f15ee1f.

--- CG/HexogonFolder/Hexagon.cs
+++ CG/HexogonFolder/Hexagon.cs
@@ -1,13 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
-using OpenTK;
 
 namespace CG.HexogonFolder
 {
     public class Hexagon
     {
-        public Vector3 Center;
-        public List<Vector3> BorderPoints;
+        public Point Center;
+        public List<Point> BorderPoints;
     }
 
 }
