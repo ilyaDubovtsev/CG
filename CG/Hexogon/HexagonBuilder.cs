39d3309610a350f216634c0d491b4316e8caf977
dubovtsev <dubovtsev@skbkontur.ru>
2018-12-26 09:27:54 +0400

    ассенизация

--- CG/Hexogon/HexagonBuilder.cs
+++ CG/Hexogon/HexagonBuilder.cs
@@ -39,17 +39,17 @@ namespace CG.HexagonFolder
 
 		public Hexagon GetNext()
 		{
+			CurrentTick += 1;
+
 			if (CurrentHexagon.Center.X - 1 + DeltaX * CurrentTick > 1 + Radius)
 			{
 				CurrentTick = 0;
 			}
 
-			CurrentTick += 1;
-
 			var newBorder = new List<Vector3>();
 			for (int i = 0; i < 6; i++)
 			{
-				newBorder.Add(TurnPoint(CurrentHexagon.BorderPoints[i], (float)DeltaPhi * CurrentTick));
+				newBorder.Add(TurnPoints(CurrentHexagon.BorderPoints[i], (float)DeltaPhi * CurrentTick));
 				newBorder[newBorder.Count - 1] = new Vector3((float)(newBorder[newBorder.Count - 1].X - 1 + DeltaX * CurrentTick), newBorder[newBorder.Count - 1].Y, 0);
 			}
 
@@ -60,26 +60,15 @@ namespace CG.HexagonFolder
 			};
 		}
 
-		private Vector3 TurnPoint(Vector3 vector, float angle)
+		private Vector3 TurnPoints(Vector3 vector, float angle)
 		{
 			return MatrixMultiplication(vector, CreateTurnMatrix(angle));
-		}
-
-		private Vector3 MoveVector(Vector3 vector, int deltaX)
-		{
-			return new Vector3((float)(vector.X + deltaX), (float)vector.Y, vector.Z);
 		}
 
 		private static float[,] CreateTurnMatrix(double angle)
 		{
 			var matrix = new float[3, 3];
-			for (int i = 0; i < 3; i++)
-			{
-				for (int j = 0; j < 3; j++)
-				{
-					matrix[i, j] = 0;
-				}
-			}
+
 			matrix[0, 0] = (float)Math.Cos(angle);
 			matrix[0, 1] = (float)Math.Sin(angle);
 			matrix[1, 0] = (float)-Math.Sin(angle);
